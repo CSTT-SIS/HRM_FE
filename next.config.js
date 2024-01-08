fd1ac3edcc160ce6c046d8a0d97eadc5c88b9813
Nguyễn Hoàng Hiệp <hoanghiepdn97@gmail.com>
2024-01-08 21:56:26 +0700

    Config prototype demo

--- next.config.js
+++ next.config.js
@@ -7,7 +7,8 @@ const nextConfig = {
         return [
             {
                 source: '/',
-                destination: '/sign-in',
+                // destination: '/sign-in',
+                destination: '/',
                 permanent: true,
             },
 
