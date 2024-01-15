769313c609d71e22c2193967244bc7beb71e1e2f
unknown <hoangtrongbinh1111>
2024-01-15 07:35:09 +0700

    show task

---
+++ src/pages/hrm/task/modal/TaskModal.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface Props {
+	[key: string]: any;
+}
+
+const TaskModal = ({ ...props }: Props) => {
+	return <div>TaskModal</div>;
+};
+
+export default TaskModal;
