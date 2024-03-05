d7013403adb20e9c384a9f47f8802564d928cd8c
Nick Vujicic <nick.vujicic@abc.xyz>
2024-03-05 23:24:11 +0700

    feat: product e2e

--- src/pages/warehouse/product/list/[id]/index.tsx
+++ src/pages/warehouse/product/list/[id]/index.tsx
@@ -16,221 +16,233 @@ import IconBackward from '@/components/Icon/IconBackward';
 import { useRouter } from 'next/router';
 
 interface Props {
-    [key: string]: any;
+	[key: string]: any;
 }
 
 const ProductModal = ({ ...props }: Props) => {
+	const router = useRouter();
+	const { t } = useTranslation();
+	const [disabled, setDisabled] = useState(false);
+	const [pageCategory, setSizeCategory] = useState<any>(1);
+	const [pageUnit, setSizeUnit] = useState<any>(1);
+	const [data, setData] = useState<any>();
+	const [dataCategoryDropdown, setDataCategoryDropdown] = useState<any>([]);
+	const [dataUnitDropdown, setDataUnitDropdown] = useState<any>([]);
 
-    const router = useRouter();
-    const { t } = useTranslation();
-    const [disabled, setDisabled] = useState(false);
-    const [pageCategory, setSizeCategory] = useState<any>(1);
-    const [pageUnit, setSizeUnit] = useState<any>(1);
-    const [data, setData] = useState<any>();
-    const [dataCategoryDropdown, setDataCategoryDropdown] = useState<any>([]);
-    const [dataUnitDropdown, setDataUnitDropdown] = useState<any>([]);
+	//get data
+	const { data: categorys, pagination: paginationCategory, isLoading: CategoryLoading } = DropdownProductCategorys({ page: pageCategory });
+	const { data: units, pagination: paginationUnit, isLoading: UnitLoading } = DropdownUnits({ page: pageUnit });
 
-    //get data
-    const { data: categorys, pagination: paginationCategory, isLoading: CategoryLoading } = DropdownProductCategorys({ page: pageCategory });
-    const { data: units, pagination: paginationUnit, isLoading: UnitLoading } = DropdownUnits({ page: pageUnit });
+	const SubmittedForm = Yup.object().shape({
+		name: Yup.string()
+			.min(2, 'Too Short!')
+			.required(`${t('please_fill_name_product')}`),
+		code: Yup.string()
+			.min(2, 'Too Short!')
+			.required(`${t('please_fill_productCode')}`),
+		unitId: new Yup.ObjectSchema().required(`${t('please_fill_unit')}`),
+		categoryId: new Yup.ObjectSchema().required(`${t('please_fill_category')}`),
+	});
+	const handleProduct = (param: any) => {
+		const query = {
+			name: param.name,
+			code: param.code,
+			unitId: Number(param.unitId.value),
+			description: param.description,
+			categoryId: Number(param.categoryId.value),
+		};
+		if (data) {
+			EditProduct({ id: data.id, ...query })
+				.then(() => {
+					handleCancel();
+					showMessage(`${t('edit_product_success')}`, 'success');
+				})
+				.catch((err) => {
+					showMessage(`${err?.response?.data?.message}`, 'error');
+				});
+		} else {
+			CreateProduct(query)
+				.then(() => {
+					handleCancel();
+					showMessage(`${t('create_product_success')}`, 'success');
+				})
+				.catch((err) => {
+					showMessage(`${err?.response?.data?.message}`, 'error');
+				});
+		}
+	};
 
-    const SubmittedForm = Yup.object().shape({
-        name: Yup.string().min(2, 'Too Short!').required(`${t('please_fill_name_product')}`),
-        code: Yup.string().min(2, 'Too Short!').required(`${t('please_fill_productCode')}`),
-        unitId: new Yup.ObjectSchema().required(`${t('please_fill_unit')}`),
-        categoryId: new Yup.ObjectSchema().required(`${t('please_fill_category')}`)
-    });
-    const handleProduct = (param: any) => {
-        const query = {
-            "name": param.name,
-            "code": param.code,
-            "unitId": Number(param.unitId.value),
-            "description": param.description,
-            "categoryId": Number(param.categoryId.value),
-        }
-        if (data) {
-            EditProduct({ id: data.id, ...query }).then(() => {
-                handleCancel();
-                showMessage(`${t('edit_product_success')}`, 'success');
-            }).catch((err) => {
-                showMessage(`${err?.response?.data?.message}`, 'error');
-            });
-        } else {
-            CreateProduct(query).then(() => {
-                handleCancel();
-                showMessage(`${t('create_product_success')}`, 'success');
-            }).catch((err) => {
-                showMessage(`${err?.response?.data?.message}`, 'error');
-            });
-        }
-    }
+	const handleCancel = () => {
+		router.push('/warehouse/product/list');
+	};
 
-    const handleCancel = () => {
-        router.push('/warehouse/product/list')
-    };
+	useEffect(() => {
+		if (paginationCategory?.page === undefined) return;
+		if (paginationCategory?.page === 1) {
+			setDataCategoryDropdown(categorys?.data);
+		} else {
+			setDataCategoryDropdown([...dataCategoryDropdown, ...categorys?.data]);
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [paginationCategory]);
 
-    useEffect(() => {
-        if (paginationCategory?.page === undefined) return;
-        if (paginationCategory?.page === 1) {
-            setDataCategoryDropdown(categorys?.data)
-        } else {
-            setDataCategoryDropdown([...dataCategoryDropdown, ...categorys?.data])
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [paginationCategory])
+	useEffect(() => {
+		if (paginationUnit?.page === undefined) return;
+		if (paginationUnit?.page === 1) {
+			setDataUnitDropdown(units?.data);
+		} else {
+			setDataUnitDropdown([...dataUnitDropdown, ...units?.data]);
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [paginationUnit]);
 
-    useEffect(() => {
-        if (paginationUnit?.page === undefined) return;
-        if (paginationUnit?.page === 1) {
-            setDataUnitDropdown(units?.data)
-        } else {
-            setDataUnitDropdown([...dataUnitDropdown, ...units?.data])
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [paginationUnit]);
+	const handleMenuScrollToBottomCategory = () => {
+		setTimeout(() => {
+			setSizeCategory(paginationCategory?.page + 1);
+		}, 1000);
+	};
 
-    const handleMenuScrollToBottomCategory = () => {
-        setTimeout(() => {
-            setSizeCategory(paginationCategory?.page + 1);
-        }, 1000);
-    }
+	const handleMenuScrollToBottomUnit = () => {
+		setTimeout(() => {
+			setSizeUnit(paginationUnit?.page + 1);
+		}, 1000);
+	};
 
-    const handleMenuScrollToBottomUnit = () => {
-        setTimeout(() => {
-            setSizeUnit(paginationUnit?.page + 1);
-        }, 1000);
-    }
+	useEffect(() => {
+		if (Number(router.query.id)) {
+			GetProduct({ id: Number(router.query.id) })
+				.then((res) => {
+					setData(res.data);
+				})
+				.catch((err) => {
+					showMessage(`${err?.response?.data?.message}`, 'error');
+				});
+		}
+	}, [router]);
 
-    useEffect(() => {
-        if (Number(router.query.id)) {
-            GetProduct({ id: Number(router.query.id) }).then((res) => {
-                setData(res.data)
-            }).catch((err) => {
-                showMessage(`${err?.response?.data?.message}`, 'error');
-            })
-        }
-    }, [router])
-
-    return (
-        <div>
-            <div className='flex justify-between header-page-bottom pb-4 mb-4'>
-                <h1 className='page-title'>{data !== undefined ? t('edit_product') : t('add_product')}</h1>
-                <Link href="/warehouse/product/list">
-                    <div className="btn btn-primary btn-sm m-1 back-button h-9" >
-                        <IconBackward />
-                        <span>
-                            {t('back')}
-                        </span>
-                    </div>
-                </Link>
-            </div>
-            <div className="p-5">
-                <Formik
-                    initialValues={
-                        {
-                            name: data ? `${data?.name}` : "",
-                            code: data ? `${data?.code}` : "",
-                            unitId: data?.unit ? {
-                                value: `${data?.unit?.id}`,
-                                label: `${data?.unit?.name}`
-                            } : "",
-                            description: data ? `${data?.description}` : "",
-                            categoryId: data?.category ? {
-                                value: `${data?.category?.id}`,
-                                label: `${data?.category?.name}`
-                            } : "",
-                        }
-                    }
-                    validationSchema={SubmittedForm}
-                    onSubmit={values => {
-                        handleProduct(values);
-                    }}
-                    enableReinitialize
-                >
-
-                    {({ errors, values, setFieldValue, submitCount }) => (
-                        <Form className="space-y-5" >
-                            <div className='flex justify-between gap-5'>
-                                <div className="w-1/2">
-                                    <label htmlFor="name" > {t('name')} < span style={{ color: 'red' }}>* </span></label >
-                                    <Field name="name" type="text" id="name" placeholder={`${t('enter_name')}`} className="form-input" />
-                                    {submitCount && errors.name ? (
-                                        <div className="text-danger mt-1"> {errors.name} </div>
-                                    ) : null}
-                                </div>
-                                <div className="w-1/2">
-                                    <label htmlFor="code" > {t('code')} < span style={{ color: 'red' }}>* </span></label >
-                                    <Field name="code" type="text" id="code" placeholder={`${t('enter_code')}`} className="form-input" />
-                                    {submitCount && errors.code ? (
-                                        <div className="text-danger mt-1"> {errors.code} </div>
-                                    ) : null}
-                                </div>
-                            </div>
-                            <div className='flex justify-between gap-5'>
-                                <div className="w-1/2">
-                                    <label htmlFor="unitId" > {t('unit')} < span style={{ color: 'red' }}>* </span></label >
-                                    <Select
-                                        id='unitId'
-                                        name='unitId'
-                                        options={dataUnitDropdown}
-                                        onMenuOpen={() => setSizeUnit(1)}
-                                        onMenuScrollToBottom={handleMenuScrollToBottomUnit}
-                                        maxMenuHeight={160}
-                                        value={values.unitId}
-                                        isLoading={UnitLoading}
-                                        onChange={e => {
-                                            setFieldValue('unitId', e)
-                                        }}
-                                    />
-                                    {submitCount && errors.unitId ? (
-                                        <div className="text-danger mt-1"> {errors.code} </div>
-                                    ) : null}
-                                </div>
-                                <div className="w-1/2">
-                                    <label htmlFor="categoryId" > {t('category')} < span style={{ color: 'red' }}>* </span></label >
-                                    <Select
-                                        id='categoryId'
-                                        name='categoryId'
-                                        options={dataCategoryDropdown}
-                                        onMenuOpen={() => setSizeCategory(1)}
-                                        onMenuScrollToBottom={handleMenuScrollToBottomCategory}
-                                        isLoading={CategoryLoading}
-                                        maxMenuHeight={160}
-                                        value={values.categoryId}
-                                        onChange={e => {
-                                            setFieldValue('categoryId', e)
-                                        }}
-                                    />
-                                    {submitCount && errors.categoryId ? (
-                                        <div className="text-danger mt-1"> {errors.categoryId} </div>
-                                    ) : null}
-                                </div>
-                            </div>
-                            <div className='flex justify-between gap-5'>
-                                <div className="w-1/2">
-                                    <label htmlFor="description" > {t('description')} </label >
-                                    <Field name="description" type="text" id="description" placeholder={`${t('enter_description')}`} className="form-input" />
-                                    {submitCount && errors.description ? (
-                                        <div className="text-danger mt-1"> {errors.description} </div>
-                                    ) : null}
-                                </div>
-                                <div className='w-1/2'></div>
-                            </div>
-                            <div className="mt-8 flex items-center justify-end ltr:text-right rtl:text-left">
-                                <button type="button" className="btn btn-outline-danger cancel-button" onClick={() => handleCancel()}>
-                                    {t('cancel')}
-                                </button>
-                                <button type="submit" className="btn btn-primary ltr:ml-4 rtl:mr-4 add-button" disabled={disabled}>
-                                    {data !== undefined ? t('update') : t('add')}
-                                </button>
-                            </div>
-
-                        </Form>
-                    )}
-                </Formik>
-            </div>
-        </div>
-    );
+	return (
+		<div>
+			<div className="header-page-bottom mb-4 flex justify-between pb-4">
+				<h1 className="page-title">{data !== undefined ? t('edit_product') : t('add_product')}</h1>
+				<Link href="/warehouse/product/list">
+					<div className="btn btn-primary btn-sm back-button m-1 h-9">
+						<IconBackward />
+						<span>{t('back')}</span>
+					</div>
+				</Link>
+			</div>
+			<div className="p-5">
+				<Formik
+					initialValues={{
+						name: data ? `${data?.name}` : '',
+						code: data ? `${data?.code}` : '',
+						unitId: data?.unit
+							? {
+									value: `${data?.unit?.id}`,
+									label: `${data?.unit?.name}`,
+							  }
+							: '',
+						description: data ? `${data?.description}` : '',
+						categoryId: data?.category
+							? {
+									value: `${data?.category?.id}`,
+									label: `${data?.category?.name}`,
+							  }
+							: '',
+					}}
+					validationSchema={SubmittedForm}
+					onSubmit={(values) => {
+						handleProduct(values);
+					}}
+					enableReinitialize
+				>
+					{({ errors, values, setFieldValue, submitCount }) => (
+						<Form className="space-y-5">
+							<div className="flex justify-between gap-5">
+								<div className="w-1/2">
+									<label htmlFor="name" data-testid={'name'}>
+										{' '}
+										{t('name')} <span style={{ color: 'red' }}>* </span>
+									</label>
+									<Field name="name" type="text" id="name" placeholder={`${t('enter_name')}`} className="form-input" />
+									{submitCount && errors.name ? <div className="mt-1 text-danger"> {errors.name} </div> : null}
+								</div>
+								<div className="w-1/2">
+									<label htmlFor="code" data-testid={'code'}>
+										{' '}
+										{t('code')} <span style={{ color: 'red' }}>* </span>
+									</label>
+									<Field name="code" type="text" id="code" placeholder={`${t('enter_code')}`} className="form-input" />
+									{submitCount && errors.code ? <div className="mt-1 text-danger"> {errors.code} </div> : null}
+								</div>
+							</div>
+							<div className="flex justify-between gap-5">
+								<div className="w-1/2">
+									<label htmlFor="unitId" data-testid={'unitId'}>
+										{' '}
+										{t('unit')} <span style={{ color: 'red' }}>* </span>
+									</label>
+									<Select
+										id="unitId"
+										name="unitId"
+										options={dataUnitDropdown}
+										onMenuOpen={() => setSizeUnit(1)}
+										onMenuScrollToBottom={handleMenuScrollToBottomUnit}
+										maxMenuHeight={160}
+										value={values.unitId}
+										isLoading={UnitLoading}
+										onChange={(e) => {
+											setFieldValue('unitId', e);
+										}}
+									/>
+									{submitCount && errors.unitId ? <div className="mt-1 text-danger"> {errors.code} </div> : null}
+								</div>
+								<div className="w-1/2">
+									<label htmlFor="categoryId">
+										{' '}
+										{t('category')} <span style={{ color: 'red' }}>* </span>
+									</label>
+									<Select
+										id="categoryId"
+										name="categoryId"
+										options={dataCategoryDropdown}
+										onMenuOpen={() => setSizeCategory(1)}
+										onMenuScrollToBottom={handleMenuScrollToBottomCategory}
+										isLoading={CategoryLoading}
+										maxMenuHeight={160}
+										value={values.categoryId}
+										onChange={(e) => {
+											setFieldValue('categoryId', e);
+										}}
+									/>
+									{submitCount && errors.categoryId ? <div className="mt-1 text-danger"> {errors.categoryId} </div> : null}
+								</div>
+							</div>
+							<div className="flex justify-between gap-5">
+								<div className="w-1/2">
+									<label htmlFor="description" data-testid={'description'}>
+										{' '}
+										{t('description')}{' '}
+									</label>
+									<Field name="description" type="text" id="description" placeholder={`${t('enter_description')}`} className="form-input" />
+									{submitCount && errors.description ? <div className="mt-1 text-danger"> {errors.description} </div> : null}
+								</div>
+								<div className="w-1/2"></div>
+							</div>
+							<div className="mt-8 flex items-center justify-end ltr:text-right rtl:text-left">
+								<button data-testid={'cancel-btn'} type="button" className="btn btn-outline-danger cancel-button" onClick={() => handleCancel()}>
+									{t('cancel')}
+								</button>
+								<button data-testid={'submit-btn'} type="submit" className="btn btn-primary add-button ltr:ml-4 rtl:mr-4" disabled={disabled}>
+									{data !== undefined ? t('update') : t('add')}
+								</button>
+							</div>
+						</Form>
+					)}
+				</Formik>
+			</div>
+		</div>
+	);
 };
 
 export default ProductModal;
