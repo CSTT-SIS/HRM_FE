4bd7328e84d7916a8169eb39d54e3e1ccfa380a5
trangcucheng <trangcucheng@gmail.com>
2024-03-21 13:29:57 +0700

    update

--- src/pages/hrm/organization-structure/NodeComponent.tsx
+++ src/pages/hrm/organization-structure/NodeComponent.tsx
@@ -5,13 +5,19 @@ interface MyNode {
     actor: string;
     name: string;
     className: string;
+    description: string;
 }
 
 const MyNodeComponent: React.FC<{ node: MyNode }> = ({ node }) => {
     const { t } = useTranslation();
     return (
-        <div className={`initechNode ${node?.className}`}>
-            <h1>{node?.name || "Unknown"}</h1>
+        <div className={`initechNode ${node?.className} inline-flex`}>
+            <img className='avatar col' src={node?.avatar} />
+            <div className="">
+                <p>{node?.name || "Unknown"}</p>
+                <p>{node?.description || ""}</p>
+            </div>
+
         </div>
     );
 };
--- src/pages/hrm/organization-structure/index.tsx
+++ src/pages/hrm/organization-structure/index.tsx
@@ -1,147 +1,36 @@
-import OrgChart from 'react-orgchart';
-import 'react-orgchart/index.css';
-import MyNodeComponent from './NodeComponent';
-import { useTranslation } from 'react-i18next';
+import {CustomOrgChartData, OrgChart, registerLicense } from '@yworks/react-yfiles-orgchart'
+import '@yworks/react-yfiles-orgchart/dist/index.css'
+import yFilesLicense from './license.json'
+import data from './data.json'
 
-const OrganizationChart: React.FC<{}> = () => {
-	const { t } = useTranslation();
-	const initechOrg = {
-		name: 'Giám đốc',
-        className: 'level-0',
-		children: [
-			{
-				name: 'Phó Giám đốc phụ trách chế biến',
-                className: 'level-1',
-				children: [
-					{
-						name: 'Ban đốc nhà máy',
-                        className: 'level-2',
-						children: [
-							{
-								name: 'Nhà máy nghiền khô',
-                                className: "level-3"
-							},
-							{
-								name: 'Nhà máy tuyến',
-                                className: "level-3"
-							},
-							{
-								name: 'Nhà máy luyện 2000 tấn, 500 tấn',
-                                className: "level-3"
-							},
-							{
-								name: 'Tổ kỹ thuật công nghệ, Tổ điện phân',
-                                className: "level-3"
-							},
-						],
-					},
-				],
-			},
-			{
-				name: 'Bộ phận Camera',
-                className: 'level-1',
-			},
-			{
-				name: 'Ban Giám sát',
-                className: 'level-1',
-			},
-			{
-				name: 'Phó Giám đốc thường trực',
-                className: 'level-1',
-				children: [
-					{
-						name: 'Phòng Phân tích, Tổ gia công mã hóa',
-                        className: 'level-2',
-					},
-					{
-						name: 'Phòng Tổ chức hành chính',
-                        className: 'level-2',
-						children: [
-							{
-								name: 'Tổ xe, Bộ phận cấp dưỡng, Bảo vệ',
-                                className: 'level-3',
-							},
-						],
-					},
-					{
-						name: 'Phòng Kế hoạch vật tư',
-                        className: 'level-2',
-						children: [
-							{
-								name: 'Tổ điện, Tổ làm ngoài, Bộ phận sửa chữa',
-                                className: 'level-3',
-							},
-						],
-					},
-					{
-						name: 'Phòng Hành chính kế toán',
-                        className: 'level-2',
-						children: [
-							{
-								name: 'Bộ phận kho vật tư',
-                                className: 'level-3',
-							},
-						],
-					},
-					{
-						name: 'Phòng Kỹ thuật',
-                        className: 'level-2',
-						children: [
-							{
-								name: 'Tổ gia công mẫu, Tổ môi trường',
-                                className: 'level-3',
-							},
-						],
-					},
-				],
-			},
-			{
-				name: 'Phó Giám đốc khai thác',
-                className: 'level-1',
-				children: [
-					{
-						name: 'Bộ phận Kỹ thuật khai thác',
-                        className: 'level-2',
-						children: [
-							{
-								name: 'Tổ lái xe Lào',
-                                className: "level-3"
-							},
-							{
-								name: 'Tổ lái xe Việt',
-                                className: "level-3"
-							},
-							{
-								name: 'Tổ máy',
-                                className: "level-3"
-							},
-							{
-								name: 'Tổ làm hồ',
-                                className: "level-3"
-							},
-							{
-								name: 'Bộ phận khoan nổ mình',
-                                className: "level-3"
-							},
-						],
-					},
-					{
-						name: 'Bộ phận Giám sát hành trình',
-                        className: "level-2"
-					},
-				],
-			},
-		],
-	};
-
-	return (
-		<div>
-            				<h1 className='uppercase company-name'>{t('organization_structure')}</h1>
-            <h1 className='uppercase company-name'>VANGTAT MINING</h1>
-			<div className="panel mt-6" style={{ overflowY: 'scroll' }}>
-				<OrgChart tree={initechOrg} NodeComponent={MyNodeComponent} />
-			</div>
-		</div>
-	);
+type Employee = {
+  id: number;
+  name?: string;
+  status?: string;
+  position?: string;
+  email?: string;
+  phone?: string;
+  description?: string;
+  avatar?: string;
 };
-export default OrganizationChart;
+
+function MyOrgChartItem(props: RenderItemProps<CustomOrgChartItem<Employee>>) {
+  const { dataItem } = props;
+  return (
+    <div
+      className={`${dataItem.name === "Eric Joplin" ? "ceo" : "employee"} item`}
+    >
+      <div>{dataItem.name}</div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <OrgChart
+      data={data}
+      renderItem={MyOrgChartItem}
+      itemSize={{ width: 150, height: 100 }}
+    ></OrgChart>
+  );
+}
